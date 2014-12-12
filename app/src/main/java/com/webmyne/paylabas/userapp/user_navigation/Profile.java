4d01dcca3a48d371274c3a64c21cd25c0b677215
rsv355 <softeng.krishna@gmail.com>
2014-12-12 20:04:22 +0530

    ll

--- app/src/main/java/com/webmyne/paylabas/userapp/user_navigation/Profile.java
+++ app/src/main/java/com/webmyne/paylabas/userapp/user_navigation/Profile.java
@@ -1,11 +1,17 @@
 package com.webmyne.paylabas.userapp.user_navigation;
 
 
+import android.annotation.TargetApi;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
@@ -20,18 +26,31 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+
+import com.android.volley.VolleyError;
 import com.gc.materialdesign.views.ButtonRectangle;
 
 import com.gc.materialdesign.views.ButtonRectangle;
 import com.gc.materialdesign.widgets.SnackBar;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.webmyne.paylabas.userapp.base.DatabaseWrapper;
+import com.webmyne.paylabas.userapp.base.MyApplication;
 import com.webmyne.paylabas.userapp.base.MyDrawerActivity;
+import com.webmyne.paylabas.userapp.custom_components.CircleDialog;
+import com.webmyne.paylabas.userapp.helpers.AppConstants;
+import com.webmyne.paylabas.userapp.helpers.CallWebService;
 import com.webmyne.paylabas.userapp.helpers.ComplexPreferences;
 import com.webmyne.paylabas.userapp.home.MyAccountFragment;
+import com.webmyne.paylabas.userapp.model.City;
 import com.webmyne.paylabas.userapp.model.User;
 import com.webmyne.paylabas_user.R;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.lang.reflect.Type;
 import java.util.Calendar;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -44,6 +63,7 @@ public class Profile extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private static final int REQUEST_CAMERA =0;
+    private static final int RESULT_OK=0;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -61,7 +81,9 @@ public class Profile extends Fragment {
     private Spinner spCountry;
     private Spinner spState;
     private Spinner spCity;
-
+    private static final int CAMERA_REQUEST = 500;
+    private static final int GALLERY_REQUEST = 300;
+    final CharSequence[] items = { "Take Photo", "Choose from Gallery" };
     private ImageView imgprofile;
     /**
      * Use this factory method to create a new instance of
@@ -141,7 +163,26 @@ public class Profile extends Fragment {
         imgprofile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                           selectImage();
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("Update Profile Photo");
+                builder.setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int item) {
+                        if (items[item].equals("Take Photo")) {
+                            Log.e("Camera ","called");
+                            Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            startActivityForResult(takePicture, CAMERA_REQUEST);
+                             Log.e("Camera ","exit");
+
+                        } else if (items[item].equals("Choose from Gallery")) {
+                            Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                            startActivityForResult(pickPhoto , GALLERY_REQUEST);
+                        }
+                    }
+                });
+                builder.show();
             }
         });
 
@@ -180,47 +221,73 @@ public class Profile extends Fragment {
                     bar.show();
                 }
                 else{
-                    processUpdateProfile();
+                  //  processUpdateProfile();
                 }
             }
         });
 
         return convertView;
     }
-    private void selectImage() {
-        final CharSequence[] items = { "Take Photo", "Choose from Gallery" };
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Update Profile Photo");
-        builder.setItems(items, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int item) {
-                if (items[item].equals("Take Photo")) {
-                    SnackBar bar = new SnackBar(getActivity(),"Camera option");
-                    bar.show();
 
-                } else if (items[item].equals("Choose from Gallery")) {
-                    SnackBar bar = new SnackBar(getActivity(),"gallery option");
-                    bar.show();
-                }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.e("inside on acitivyty","inside on activity");
+        if (requestCode == CAMERA_REQUEST) {
+            if (resultCode == getActivity().RESULT_OK) {
+
+                Bitmap bmp = (Bitmap) data.getExtras().get("data");
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+                bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+
+                // Convert ByteArray to Bitmap::
+
+                Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0,
+                        byteArray.length);
+
+                imgprofile.setImageBitmap(bitmap);
+               // imgprofile.getLayoutParams().width=150;
+               // imgprofile.getLayoutParams().height=150;
+
             }
-        });
-        builder.show();
+            else{
+                SnackBar bar = new SnackBar(getActivity(),"Error to load Image from Camera");
+                bar.show();
+            }
+
+        }
+        else  if (requestCode == GALLERY_REQUEST) {
+            if (resultCode == getActivity().RESULT_OK) {
+                Uri selectedImage = data.getData();
+                String[] filePath = { MediaStore.Images.Media.DATA };
+                Cursor c = getActivity().getContentResolver().query(selectedImage,filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+                c.close();
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+
+                Log.e("path of image from gallery......******.........", picturePath+"");
+              //  imgprofile.setBackground(andro);
+                imgprofile.setBackground(null);
+                imgprofile.setImageBitmap(thumbnail);
+
+            }
+            else{
+                SnackBar bar = new SnackBar(getActivity(),"Error to load Image from Gallery");
+                bar.show();
+            }
+        }
     }
     public void intView(){
-        ComplexPreferences complexPreferences = ComplexPreferences.getComplexPreferences(getActivity(), "user_pref", 0);
-        User user = complexPreferences.getObject("current_user", User.class);
-        edFirstName.setText(user.FName.toString());
-        edLastName.setText(user.LName.toString());
-//    edBirthdate.setText(user.DOBString.toString());
-        edEmail.setText(user.EmailID.toString());
-        //  Log.e("erro-add", user.Address.toString());
-        //edAddress.setText(user.Address.toString());
-//    edZipcode.setText(user.Zip.toString());
-        //edCountryCode.setText(user.MobileCountryCode);
-        edMobileno.setText(user.MobileNo.toString());
 
-    }
+        processUpdateProfile();
+   }
 
     public boolean isZipcodeMatch(EditText param1){
         boolean isMatch = false;
@@ -238,8 +305,74 @@ public class Profile extends Fragment {
         return isEmpty;
     }
 
+public void get_data(){
+ /*   final CircleDialog circleDialog=new CircleDialog(getActivity().getApplicationContext(),0);
+    circleDialog.setCancelable(true);
+    circleDialog.show();
+*/
+
+    ComplexPreferences complexPreferences = ComplexPreferences.getComplexPreferences(getActivity(), "user_pref", 0);
+    User user = complexPreferences.getObject("current_user", User.class);
+    Log.e("in getdata","start");
+
+    new CallWebService(AppConstants.GET_USER_PROFILE +user.UserID,CallWebService.TYPE_JSONARRAY) {
+
+        @Override
+        public void response(String response) {
+           // circleDialog.dismiss();
+            Log.e("res of profile",response);
+            User currentUser = new GsonBuilder().create().fromJson(response,User.class);
+            ComplexPreferences complexPreferences = ComplexPreferences.getComplexPreferences(getActivity(), "user_pref", 0);
+            complexPreferences.putObject("current_user", currentUser);
+            complexPreferences.commit();
+        }
+
+        @Override
+        public void error(VolleyError error) {
+
+         //  circleDialog.dismiss();
+        }
+    }.start();
+
+}
     public void processUpdateProfile(){
 
+           new AsyncTask<Void,Void,Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+
+                 get_data();
+
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                ComplexPreferences complexPreferences = ComplexPreferences.getComplexPreferences(getActivity(), "user_pref", 0);
+                User user1 = complexPreferences.getObject("current_user", User.class);
+
+                   edFirstName.setText(user1.FName.toString());
+                   edLastName.setText(user1.LName.toString());
+                  // edBirthdate.setText(user.DOBString.toString());
+                   edEmail.setText(user1.EmailID.toString());
+                  // edAddress.setText(user.Address.toString());
+                  // edZipcode.setText(user.Zip.toString());
+                  // edCountryCode.setText(user.MobileCountryCode);
+                   edMobileno.setText(user1.MobileNo.toString());
+
+            }
+        }.execute();
+
+
+
+
+
+
+
+
+        /*
         SnackBar bar = new SnackBar(getActivity(),"Profile Update Sucessfully");
         bar.show();
 
@@ -247,8 +380,9 @@ public class Profile extends Fragment {
         FragmentTransaction ft = manager.beginTransaction();
         ft.replace(R.id.main_container,new MyAccountFragment());
         ft.commit();
+*/
 
-
+        // end od processupdate profile
     }
 
 }
