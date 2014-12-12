f6a47dcc7ae25eea0076619ffaeebd8456e9e3a8
Nirav <niravnvk@gmail.com>
2014-12-12 12:54:59 +0530

    nvk

--- app/src/main/java/com/webmyne/paylabas/userapp/giftcode/RedeemGCFragment.java
+++ app/src/main/java/com/webmyne/paylabas/userapp/giftcode/RedeemGCFragment.java
@@ -1,45 +1,44 @@
 package com.webmyne.paylabas.userapp.giftcode;
 
-
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import com.android.volley.VolleyError;
+import com.gc.materialdesign.widgets.SnackBar;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.webmyne.paylabas.userapp.custom_components.CircleDialog;
+import com.webmyne.paylabas.userapp.helpers.AppConstants;
+import com.webmyne.paylabas.userapp.helpers.CallWebService;
+import com.webmyne.paylabas.userapp.helpers.ComplexPreferences;
+import com.webmyne.paylabas.userapp.model.GiftCode;
+import com.webmyne.paylabas.userapp.model.User;
 import com.webmyne.paylabas_user.R;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RedeemGCFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class RedeemGCFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ListView listRedeemGC;
+    private ArrayList<GiftCode> giftCodes;
+    private ArrayList<GiftCode> redeemGiftCodesList;
+    private CircleDialog circleDialog;
+    private User user;
+    private GCAdapter gcAdapter;
 
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RedeemGCFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static RedeemGCFragment newInstance(String param1, String param2) {
         RedeemGCFragment fragment = new RedeemGCFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
@@ -50,18 +49,146 @@ public class RedeemGCFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_redeem_gc, container, false);
+        View convertView = inflater.inflate(R.layout.fragment_redeem_gc, container, false);
+        listRedeemGC = (ListView) convertView.findViewById(R.id.listRedemGC);
+        listRedeemGC.setEmptyView(convertView.findViewById(R.id.redeemEmptyView));
+        return convertView;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        ComplexPreferences complexPreferences = ComplexPreferences.getComplexPreferences(getActivity(), "user_pref", 0);
+        user = complexPreferences.getObject("current_user", User.class);
+        fetchRedeemGCList();
+    }
+
+    private void fetchRedeemGCList() {
+//        Log.e("fetchRedeemGCList","fetchRedeemGCList");
+        circleDialog = new CircleDialog(getActivity(), 0);
+        circleDialog.setCancelable(true);
+        circleDialog.show();
+
+        new CallWebService(AppConstants.GIFTCODE_LIST + user.UserID, CallWebService.TYPE_JSONARRAY) {
+
+            @Override
+            public void response(String response) {
+
+                circleDialog.dismiss();
+//                Log.e("Response GC List ", response);
+                Type listType = new TypeToken<List<GiftCode>>() {
+                }.getType();
+                giftCodes = new GsonBuilder().create().fromJson(response, listType);
+                fillUpRedeemGCs(giftCodes);
+
+            }
+
+            @Override
+            public void error(VolleyError error) {
+
+                circleDialog.dismiss();
+                SnackBar bar = new SnackBar(getActivity(), "Sync Error. Please Try again");
+                bar.show();
+
+            }
+        }.start();
+
+    }
+
+    private void fillUpRedeemGCs(ArrayList<GiftCode> giftCodes) {
+
+        redeemGiftCodesList = new ArrayList<GiftCode>();
+
+        for (GiftCode giftCode : giftCodes) {
+            if (giftCode.isUsed == true) {
+                redeemGiftCodesList.add(giftCode);
+            }
+        }
+
+        setRedeemGc(redeemGiftCodesList);
+    }
+
+    private void setRedeemGc(final ArrayList<GiftCode> redeemGiftCodesList) {
+
+        if(redeemGiftCodesList !=null) {
+            gcAdapter = new GCAdapter(getActivity(), redeemGiftCodesList);
+            listRedeemGC.setAdapter(gcAdapter);
+            gcAdapter.notifyDataSetChanged();
+        }
+    }
+
+    private class GCAdapter extends BaseAdapter {
+
+        private ArrayList<GiftCode> redeemList;
+        private Context context;
+        private LayoutInflater mInflater;
+        private ViewHolder holder;
+
+        public GCAdapter(FragmentActivity activity, ArrayList<GiftCode> redeemGiftCodesList) {
+            this.context = activity;
+            this.redeemList = redeemGiftCodesList;
+        }
+
+        @Override
+        public int getCount() {
+
+            return redeemList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        private class ViewHolder {
+            TextView txtGcItemTitleName, txtGcItemAmount, txtGcItemMobile, txtGcItemDate;
+            ImageView imgCombine;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+
+            mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+            if (convertView == null) {
+
+                convertView = mInflater.inflate(R.layout.item_mygc_list, parent, false);
+
+                holder = new ViewHolder();
+                holder.txtGcItemTitleName = (TextView) convertView.findViewById(R.id.txtGcItemTitleName);
+                holder.txtGcItemAmount = (TextView) convertView.findViewById(R.id.txtGcItemAmount);
+                holder.txtGcItemMobile = (TextView) convertView.findViewById(R.id.txtGcItemMobile);
+                holder.txtGcItemDate = (TextView) convertView.findViewById(R.id.txtGcItemDate);
+                holder.imgCombine = (ImageView)convertView.findViewById(R.id.imgCombine);
+
+                convertView.setTag(holder);
+
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            holder.imgCombine.setVisibility(View.GONE);
+            holder.txtGcItemAmount.setText(getResources().getString(R.string.euro)+" "+redeemList.get(position).GCAmount);
+            holder.txtGcItemDate.setText(redeemList.get(position).GCGeneratedDateString);
+            holder.txtGcItemTitleName.setText(redeemList.get(position).SendBy.substring(0, 1).toUpperCase() + redeemList.get(position).SendBy.substring(1));
+            holder.txtGcItemMobile.setText("+"+redeemList.get(position).CountryCode+" "+redeemList.get(position).SenderMob);
+
+            return convertView;
+        }
+
+    }
 
 }
